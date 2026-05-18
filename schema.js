a5427cda764135551baf9b20b3da5e1ed6614ad8
Ananta <anantapandey0902@gmail.com>
2026-05-18 19:03:48 +0530

    Updated navbar alignment

--- schema.js
+++ schema.js
@@ -1,17 +1,20 @@
 const Joi = require('joi');
 
 const listingSchema = Joi.object({
-    listing: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        location: Joi.string().required(),
-        country: Joi.string().required(),
-        price: Joi.number().required().min(0),
-      image: Joi.object({
-  url: Joi.string(),
-  filename: Joi.string()
-}).optional()
-})
+  listing: Joi.object({
+    title: Joi.string().required(),
+
+    description: Joi.string().required(),
+
+    location: Joi.string().required(),
+
+    country: Joi.string().required(),
+
+    price: Joi.number().required().min(0),
+
+    imageUrl: Joi.string().allow("", null),
+
+  }).required().unknown(true)
 });
 
 const reviewSchema = Joi.object({
